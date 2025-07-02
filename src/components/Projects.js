ff6e3df2b078fbf49b14b8fd243e7e4451501dd6
Ashen Ruvinda <138576609+AshenRuvinda@users.noreply.github.com>
2025-07-02 13:08:12 +0530

    project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import '../styles/App.css';
+import './Projects.css';
 
 const Projects = () => {
   const [flippedCard, setFlippedCard] = useState(null);
